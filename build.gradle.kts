66fb5a705b871d679e1a9041f947a88f76d40837
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-07-31 01:10:09 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,11 @@ defaultTasks("build", "shadowJar")
 
 allprojects {
     group = "de.natrox"
-    version = "1.0.0-SNAPSHOT"
+    version = "1.0"
 
     repositories {
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
@@ -19,17 +20,21 @@ dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha7")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testImplementation("org.junit.platform:junit-platform-suite-api:1.8.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-suite-engine:1.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.platform:junit-platform-suite-api:1.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-suite-engine:1.9.0")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.withType<JavaCompile> {
     sourceCompatibility = JavaVersion.VERSION_17.toString()
     targetCompatibility = JavaVersion.VERSION_17.toString()
-    // options
     options.encoding = "UTF-8"
     options.isIncremental = true
 }
